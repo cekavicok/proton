5c41c5b6dbf6b4b7a000441eb70cdff876086ee2
Gordan Djordjevic <dg220679@rti.lab>
2025-06-24 19:33:27 +0200

    dadadadad

---
+++ proton.c
@@ -0,0 +1,25 @@
+int sub(int a, int b) { 
+    return a - b;
+}
+
+int main(){
+    int a, b;
+    scanf("%d %d", &a, &b);
+    char operacija;
+    scanf("%c", &operacija);
+    switch (operacija) {
+    case '-':
+        
+        break;
+    case '+':
+        // code block
+        break;
+    case '*':
+        // code block
+        break;
+    case '/':
+        // code block
+        break;
+}
+
+} 
